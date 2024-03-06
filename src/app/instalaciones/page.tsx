a9202776fae95e2907ce6a5e97a4a9337814d22a
adrianhdez929 <adrianhdez929@gmail.com>
2024-03-05 22:57:50 -0500

    add base layouts and first level routes with base pages

---
+++ src/app/instalaciones/page.tsx
@@ -0,0 +1,116 @@
+
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import facilities from './data.json'
+
+const people = [
+  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+  // More people...
+]
+
+export default function FacilitiesPage() {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Aeropuertos</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            Lista de las instalaciones agregadass a la base de datos.
+          </p>
+        </div>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <button
+            type="button"
+            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Agregar instalacion
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    <a href="#" className="group inline-flex">
+                      Id
+                      <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <a href="#" className="group inline-flex">
+                      Aeropuerto
+                      <span className="ml-2 flex-none rounded bg-gray-100 text-gray-900 group-hover:bg-gray-200">
+                        <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <a href="#" className="group inline-flex">
+                      Nombre
+                      <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                        <ChevronDownIcon
+                          className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <a href="#" className="group inline-flex">
+                      Tipo
+                      <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                        <ChevronDownIcon
+                          className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    <a href="#" className="group inline-flex">
+                      Ubicacion
+                      <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+                        <ChevronDownIcon
+                          className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible"
+                          aria-hidden="true"
+                        />
+                      </span>
+                    </a>
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {facilities.map((facility) => (
+                  <tr key={facility.id}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      {facility.id}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{facility.id_aero}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{facility.nombre}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{facility.tipo}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{facility.ubicacion}</td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm sm:pr-0">
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Editar <span className="sr-only">, {facility.id}</span>
+                      </a>
+                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                        Eliminar <span className="sr-only">, {facility.id}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
